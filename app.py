e4f1c4c9f8f1af68b8225555ca7835767208a53c
GhazaGG <ghazagg7@gmail.com>
2023-02-17 18:21:42 +0700

    fixing slot game

--- app.py
+++ app.py
@@ -1,33 +1,51 @@
 MAX_LINE = 3
+MAX_BET = 100
+MIN_BET = 1
 
 
 def deposit():
     while True:
-        jumlah = input ("Masukan jumlah yang akan anda deposit: ")
-        if jumlah.isdigit():
-            jumlah = int(jumlah)
+        amount = input ("What would you like to deposit? :$ ")
+        if amount.isdigit():
+            amount = int(amount)
             break
         else:
-            print("Masukan nilai yang benar!")
-    return jumlah
+            print("Please enter a valid number!")
+    return amount
 
 def line():
     while True:
-        line = input ("Masukan jumlah baris yang anda bet! : ( 1-"+ str(MAX_LINE)+ ")?")
+        line = input ("What line would you like to bet? : ( 1-"+ str(MAX_LINE)+ ")?")
         if line.isdigit():
             line = int(line)
             if 1 <= line <= MAX_LINE:
                 break
             else:
-                print("Tolong masukan jumlah yang benar")
+                print("Please enter a valid number!")
         else:
-            print("Tolong masukan jumlah yang benar!")
+            print("Please enter a valid number!")
     return line 
 
+def bet():
+    while True:
+        amount = input("What would you like to bet?")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}")
+        else:
+            print("Please enter a number!")
 
-saldo = deposit()
-line = line()
-print("Jumlah saldo anda: ", saldo)
-print("Line yang anda bet", line)
+
+def main():
+    balance = deposit()
+    lines = line()
+    bet = bet()
+    total_bet = bet * lines
+    print(f"You are betting ${bet} on {lines} line. Total bet is equal to: ${total_bet}")
+    print(balance, lines)
 
 
+main()
